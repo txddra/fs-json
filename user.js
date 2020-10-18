2ec031c75c0afa38a894e16d695563e8fac394ad
LaToddra Belton <latoddra.belton@codeimmersives.com>
2020-10-18 12:52:05 -0400

    logs select info

--- user.js
+++ user.js
@@ -3,7 +3,18 @@ const axios = require('axios');
 const url ='https://jsonplaceholder.typicode.com/users';
 
 function use (url){
-    axios.get(url).then((entry)=>console.log(entry.data)).catch((err)=> console.log(err))
+    axios.get(url).then((entry)=>{
+        let list =entry.data
+        list.forEach(element => {
+            console.log(`${element.name}`);
+            console.log(`Address: ${element.address.street} ${element.address.suite}`);
+            console.log(`${element.address.city} ${element.address.zipcode}`);
+            console.log(`Phone: ${element.phone}`);
+            console.log(`Email: ${element.email}`)
+        });
+    
+    
+    }).catch((err)=> console.log(err))
 }
 
 use(url)
